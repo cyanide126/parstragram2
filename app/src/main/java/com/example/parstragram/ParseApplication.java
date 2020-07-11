63f863e38f465bd569a66ff68f61af63f3229020
cyanide126 <cyada2000@gmail.com>
2020-07-11 10:46:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/parstragram/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.parstragram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("yada-parstagram") // should correspond to APP_ID env variable
+                .clientKey("codepath")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("https://yada-parstagram.herokuapp.com/parse/").build());
+    }
+}
